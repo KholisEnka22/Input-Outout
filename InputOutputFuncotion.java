f594ddb240b8ca4ebe0e3cd8aa54d6a77aae1c0a
KholisEnka22 <enka.run2206@gmail.com>
2020-11-07 06:29:18 +0700

    Tugas Pertama

---
+++ InputOutputFuncotion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InputOutput;
+
+import java.util.Scanner;
+
+public class InputOutputFuncotion {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumalahan sederhana");
+        System.out.print("Masukan Angka pertama : ");
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result);
+    }
+    
+}
